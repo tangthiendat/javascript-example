7d77095f00e7a75c64f9a331dd1fc491c0909a41
tangthiendat <thiendattang@gmail.com>
2023-09-24 20:16:51 +0700

    Type conversion and coercion

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -58,3 +58,10 @@ if (BMIMark > BMIJohn) {
 } else {
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }
+
+/** Type Conversion and Coercion */
+console.log("9" - "5"); //=> 4
+console.log("19" - "13" + "17"); // => 617
+console.log("19" - "13" + 17); //=> 23
+console.log("123" < 57); //=> false
+console.log(5 + 6 + "4" + 9 - 4 - 2); //=> 1143
