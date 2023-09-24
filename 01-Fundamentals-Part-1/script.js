913eac103b0789e47f2795d6a86f98ebaaf95097
tangthiendat <thiendattang@gmail.com>
2023-09-24 10:44:46 +0700

    Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -7,3 +7,12 @@ let population = 97;
 // console.log(country);
 // console.log(continent);
 // console.log(population);
+
+/** Data Types*/
+let isIsland = true;
+let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
