b94d0edbd5b86ce019aaaefffe0d929c7118070c
tangthiendat <thiendattang@gmail.com>
2023-09-24 10:43:35 +0700

    Values and Variables

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,9 @@
+/** Values and Variables */
+
+let country = "Vietnam";
+let continent = "Asia";
+let population = 97;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
