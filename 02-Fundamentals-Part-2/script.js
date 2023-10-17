73d62965da50bdbca4d9a01a94ca26186f2baf34
Tang Thien Dat <thiendattang@gmail.com>
2023-10-17 10:26:02 +0700

    Function Declarations vs Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -7,6 +7,27 @@ function describeCountry(country, population, capitalCity) {
 
 const descVietnam = describeCountry("Vietnam", 97, "Ha Noi");
 const descAmerica = describeCountry("America", 333, "New York");
-const descChina = describeCountry("China", 1410, "Beijing");
+const descChina = describeCountry("China", 1412, "Beijing");
 
 console.log(descVietnam, descAmerica, descChina);
+
+/** Function Declarations vs Expressions */
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const percVietnam1 = percentageOfWorld1(97);
+const percAmerica1 = percentageOfWorld1(333);
+const percChina1 = percentageOfWorld1(1412);
+
+console.log(percVietnam1, percAmerica1, percChina1);
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+};
+
+const percVietnam2 = percentageOfWorld2(97);
+const percAmerica2 = percentageOfWorld2(333);
+const percChina2 = percentageOfWorld2(1412);
+
+console.log(percVietnam2, percAmerica2, percChina2);
