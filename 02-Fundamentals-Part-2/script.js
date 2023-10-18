8bec284fb1a74a26279be9da9d3d9fcf91ac25f9
Tang Thien Dat <thiendattang@gmail.com>
2023-10-18 15:48:27 +0700

    Coding challenge #2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -101,3 +101,14 @@ if (!neighbors.includes("Germany")) {
 
 neighbors[neighbors.indexOf("China")] = "Chinaaaaaaa";
 console.log(neighbors);
+
+/** Coding challenge #2 */
+const calcTip = (bill) => (bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+console.log(tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
