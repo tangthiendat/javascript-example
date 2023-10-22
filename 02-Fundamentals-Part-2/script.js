0f1049c5d6b4eeaec19c0ea9bab1b4ca86b00d4b
Tang Thien Dat <thiendattang@gmail.com>
2023-10-22 14:17:00 +0700

    Coding challenge #4

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -102,14 +102,14 @@ console.log(neighbors);
 
 /** Coding challenge #2 */
 const calcTip = (bill) => (bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
-console.log(calcTip(100));
+// console.log(calcTip(100));
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
-console.log(tips);
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+// console.log(tips);
 
-const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(total);
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(total);
 
 /** Introduction to Objects */
 const myCountry = {
@@ -205,3 +205,25 @@ while (i < populations.length) {
     i++;
 }
 console.log(percentages3);
+
+/** Coding challenge #4 */
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(bills[i] + tip);
+}
+console.log(tips);
+console.log(totals);
+
+function calcAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
