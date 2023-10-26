cd1c76cdd876efcca9994643b77d884ad76067c7
Tang Thien Dat <thiendattang@gmail.com>
2023-10-26 10:22:44 +0700

    Implement highscore

--- 04-Guess-My-Number/script.js
+++ 04-Guess-My-Number/script.js
@@ -1,6 +1,7 @@
 'use strict';
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -11,6 +12,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high';
