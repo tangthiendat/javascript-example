fe2791a2c06feaae6bed62a01bf324629f355e2d
Tang Thien Dat <thiendattang@gmail.com>
2023-10-26 21:37:00 +0700

    Roll the dice

---
+++ 06-Pig-Game/script.js
@@ -0,0 +1,35 @@
+'use strict';
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //Generating random dice roll
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  //Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    //Switch to next player
+  }
+});
