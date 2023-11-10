c87d5f4d06668fb33d2025ae5b023d7fa2f580f9
Tang Thien Dat <thiendattang@gmail.com>
2023-11-10 07:28:01 +0700

    Coding challenge #3

--- 07-Data-Structures-Operators/script.js
+++ 07-Data-Structures-Operators/script.js
@@ -457,3 +457,44 @@ for (const [key, value] of question) {
 const answer = 3;
 console.log(answer);
 console.log(question.get(answer === question.get('correct')));
+
+//Convert map to array
+console.log([...question]);
+// console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/** Coding challenge #3 */
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1.
+const events = [...new Set([...gameEvents.values()])];
+console.log(events);
+
+//2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3.
+const matchTime = [...gameEvents.keys()].pop();
+console.log(matchTime);
+console.log(
+  `An event happened, on average, every ${matchTime / gameEvents.size} minutes`
+);
+
+//4.
+for (const [key, value] of gameEvents) {
+  console.log(`[${key < 45 ? 'FIRST' : 'SECOND'} HALF] ${key}: ${value}`);
+}
