6308d8478fab6ede32d192a300c042d7afd2bc03
Tang Thien Dat <thiendattang@gmail.com>
2023-11-14 09:00:01 +0700

    Working with Strings - Part 1

--- 07-Data-Structures-Operators/script.js
+++ 07-Data-Structures-Operators/script.js
@@ -498,3 +498,41 @@ console.log(
 for (const [key, value] of gameEvents) {
   console.log(`[${key < 45 ? 'FIRST' : 'SECOND'} HALF] ${key}: ${value}`);
 }
+
+/** Working with Strings - Part 1 */
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(airline[0]);
+console.log(airline[1]);
+console.log(airline[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat');
+  } else {
+    console.log('You got lucky');
+  }
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
