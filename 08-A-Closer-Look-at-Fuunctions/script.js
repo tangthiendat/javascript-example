02eeb074021b72d1ed5b5f24bc5752c7a8786e2c
Tang Thien Dat <thiendattang@gmail.com>
2023-12-13 13:51:11 +0700

    How passing arguments work Value vs. Reference

--- 08-A-Closer-Look-at-Fuunctions/script.js
+++ 08-A-Closer-Look-at-Fuunctions/script.js
@@ -4,12 +4,12 @@
 const bookings = [];
 
 const createBooking = function (
-  fligtNum,
+  flightNum,
   numPassengers = 1,
   price = 199 * numPassengers
 ) {
   const booking = {
-    fligtNum,
+    flightNum,
     numPassengers,
     price,
   };
@@ -20,3 +20,32 @@ createBooking('LH123');
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+
+/** How passing arguments work Value vs. Reference */
+const flight = 'LH234';
+const dat = {
+  name: 'Thien Dat',
+  passport: 14823942343,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 14823942343) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+// checkIn(flight, dat);
+console.log(flight);
+console.log(dat);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(dat);
+// checkIn(flight, dat);
+console.log(dat);
