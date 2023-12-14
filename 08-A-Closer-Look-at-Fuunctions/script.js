11de0227352f39404442f5c876aa6f0629ed3bbe
Tang Thien Dat <thiendattang@gmail.com>
2023-12-14 20:38:05 +0700

    The bind method

--- 08-A-Closer-Look-at-Fuunctions/script.js
+++ 08-A-Closer-Look-at-Fuunctions/script.js
@@ -130,3 +130,42 @@ const flightData = [583, 'George Cooper'];
 book.apply(swiss, flightData);
 
 book.call(swiss, ...flightData);
+
+/** The bind method */
+//Create a new function that has the this keyword point to eurowings
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(155, 'Steven Williams');
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Tang Thien Dat');
+bookEW23('Martha Cooper');
+
+//With event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+//Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
