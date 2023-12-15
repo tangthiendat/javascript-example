131d0719ab49d3ba7f4cd658160b941186d5d354
Tang Thien Dat <thiendattang@gmail.com>
2023-12-15 19:57:45 +0700

    Coding challenge #1

--- 08-A-Closer-Look-at-Functions/script.js
+++ 08-A-Closer-Look-at-Functions/script.js
@@ -169,3 +169,37 @@ const addTaxRate = function (rate) {
 
 const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
+
+/** Coding challenge #1 */
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}(Write option number)`
+      )
+    );
+    if (typeof answer === 'number' && answer < this.answers.length) {
+      this.answers[answer]++;
+    }
+    this.displayResult();
+    this.displayResult('string');
+  },
+  displayResult(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else {
+      console.log(`Poll results are ${this.answers.join(', ')}.`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] });
