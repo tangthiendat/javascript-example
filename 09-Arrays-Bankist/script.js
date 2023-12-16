4bc7ba183e6e1efc079a0d0c72a014c38d9fa642
Tang Thien Dat <thiendattang@gmail.com>
2023-12-16 20:31:46 +0700

    Looping Arrays forEach

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /** Simple Array Methods */
@@ -117,3 +117,21 @@ console.log(arr.at(-1));
 
 console.log('dat'.at(0));
 console.log('dat'.at(-1));
+
+/** Looping Arrays forEach */
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log('----FOREACH----');
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
