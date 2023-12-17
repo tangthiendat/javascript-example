5ed04c72b050880b1c398fe7301e07066b45f2dd
Tang Thien Dat <thiendattang@gmail.com>
2023-12-17 20:52:34 +0700

    The map Method

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -189,3 +189,19 @@ console.log('---DATA 1---');
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 console.log('---DATA 2---');
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+/** The map Method */
+const eurToUsd = 1.1;
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsDescriptions = movements.map((mov, i) => {
+  return `Movement ${i + 1}: You ${
+    mov > 0 ? 'deposited' : 'withdrew'
+  } ${Math.abs(mov)}`;
+});
+console.log(movementsDescriptions);
