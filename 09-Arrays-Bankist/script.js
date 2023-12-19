b6b31cdd604dd737e1530d3652534affd0a0aaf6
Tang Thien Dat <thiendattang@gmail.com>
2023-12-19 16:06:12 +0700

    Coding challenge #3

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -279,3 +279,14 @@ const totalDepositUSD = movements
   //   .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositUSD);
+
+/** Coding challenge #3 */
+const calcAverageHumanAge2 = function (ages) {
+  const avgHumanAge = ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  return avgHumanAge;
+};
+console.log(calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]));
