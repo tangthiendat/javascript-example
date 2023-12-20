87b77b218016f61b0ceb64c8b826514b59c14578
Tang Thien Dat <thiendattang@gmail.com>
2023-12-20 20:29:42 +0700

    The some and every Method

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -161,6 +161,18 @@ btnTransfer.addEventListener('click', function (event) {
   }
 });
 
+/** some and every */
+btnLoan.addEventListener('click', function (event) {
+  event.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //Add movement
+    currentAccount.movements.push(amount);
+    inputLoanAmount.value = '';
+    updateUI(currentAccount);
+  }
+});
+
 /** The findIndex Method */
 btnClose.addEventListener('click', function (event) {
   event.preventDefault();
@@ -373,3 +385,21 @@ console.log(firstWithdrawal);
 
 const account = accounts.find(acc => (acc.owner = 'Jessica Davis'));
 console.log(account);
+
+/** some and every */
+//EQUALITY
+console.log(movements.includes(-130));
+//SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+//EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+//Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
