5896a4db9ca90155a2639b3368ffa7c7a209cc6c
Tang Thien Dat <thiendattang@gmail.com>
2023-12-21 19:21:11 +0700

    More Ways of Creating and Filling Arrays

--- 09-Arrays-Bankist/script.js
+++ 09-Arrays-Bankist/script.js
@@ -464,3 +464,30 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+/** More Ways of Creating and Filling Arrays */
+const array = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+const x = new Array(7);
+console.log(x);
+// x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+array.fill(23, 2, 6);
+console.log(array);
+
+//Array.from()
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+});
