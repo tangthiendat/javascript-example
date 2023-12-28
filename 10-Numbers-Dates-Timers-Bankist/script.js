de4fb0ecdafb02703a258cf5028b10d54dd82a60
Tang Thien Dat <thiendattang@gmail.com>
2023-12-28 18:20:12 +0700

    Creating Dates

--- 10-Numbers-Dates-Timers-Bankist/script.js
+++ 10-Numbers-Dates-Timers-Bankist/script.js
@@ -349,3 +349,34 @@ console.log(`${huge} is VERY BIG!!!`);
 //Divisions
 console.log(10n / 3n);
 console.log(10 / 3);
+
+/** Creating Dates */
+// const now = new Date();
+// console.log(now);
+// console.log(new Date('Thu Dec 28 2023 10:17:45'));
+// console.log(new Date('December 24, 2023'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 22, 5));
+// console.log(new Date(2037, 10, 31));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+//Working with date
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay()); //day of week
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
