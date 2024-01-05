62d284feed63113711f255f5525428266f1880e7
Tang Thien Dat <thiendattang@gmail.com>
2024-01-05 18:36:56 +0700

    Prototypes

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -4,15 +4,26 @@ const Person = function (firstName, birthYear) {
   //Instance properties
   this.firstName = firstName;
   this.birthYear = birthYear;
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
 };
 
 const dat = new Person('Dat', 2003);
 console.log(dat);
 console.log(dat instanceof Person);
+const matilda = new Person('Matilda', 2017);
 //1. New {} is created
 //2. function is called, this = {}
 //3. {} linked to prototype
 //4. function automatically return {}
+
+/** Prototypes */
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+dat.calcAge();
+matilda.calcAge();
+
+console.log(dat.__proto__);
+console.log(dat.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(dat));
