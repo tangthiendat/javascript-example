bf98925f3fc94bad9868f0aa4a8131a10ad426dc
Tang Thien Dat <thiendattang@gmail.com>
2024-01-06 20:22:00 +0700

    Getters and setters

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -72,14 +72,26 @@ mercedes.brake();
 
 //Class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
   //Method will be added to .prototype property
   calcAge() {
     console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+  //Set a property that already exists
+  set fullName(name) {
+    if (name.includes('')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name`);
+    }
   }
 }
 const jessica = new PersonCl('Jessica', 1996);
@@ -96,3 +108,19 @@ console.log(jessica.__proto__ === PersonCl.prototype);
 //1. Classes are NOT hoisted
 //2. Classes are first-class citizens
 //3. Classes are executed in strict mode
+
+/** Getters and Setters */
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
