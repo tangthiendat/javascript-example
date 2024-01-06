089c68d1b348d102031199fbf61eebef41431621
Tang Thien Dat <thiendattang@gmail.com>
2024-01-06 10:49:50 +0700

    Coding challenge #1

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -41,3 +41,27 @@ Array.prototype.unique = function () {
   return [...new Set(this)];
 };
 console.log(arr.unique());
+
+/** Coding challenge #1 */
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`This ${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`This ${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.brake();
+mercedes.accelerate();
+mercedes.brake();
