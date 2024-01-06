54f5481feea5ccb83dc1edcf091eb3f36d525125
Tang Thien Dat <thiendattang@gmail.com>
2024-01-06 11:55:17 +0700

    ES6 Classes

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -65,3 +65,34 @@ bmw.accelerate();
 bmw.brake();
 mercedes.accelerate();
 mercedes.brake();
+
+/** ES6 Classes */
+//Class expression
+// const personCl = class {};
+
+//Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  //Method will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+// jessica.greet();
+
+//1. Classes are NOT hoisted
+//2. Classes are first-class citizens
+//3. Classes are executed in strict mode
