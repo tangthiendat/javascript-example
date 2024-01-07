2bc12d932fd1740836cc73a94531066cd8f00b95
Tang Thien Dat <thiendattang@gmail.com>
2024-01-07 08:07:25 +0700

    Inheritance Between Classes: ES6 Classes

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -187,3 +187,28 @@ ford.accelerate();
 ford.brake();
 ford.speedUS = 50;
 console.log(ford);
+
+/** Inheritance Between "Classes": ES6 Classes */
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    //Always need to happen first
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years, but now as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
